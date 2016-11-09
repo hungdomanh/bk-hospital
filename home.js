2ecbf77be7451750ca1958f6a598d4fb473de2a9
lampvhy <lampv.hust@gmail.com>
2016-11-09 22:38:01 +0700

    lam

---
+++ home.js
@@ -0,0 +1,17 @@
+
+
+   $(document).ready(function() {
+
+   	$("#change_").on("click", function(){
+   		window.location="hospital2.html" ;
+     
+    });
+    $("#ttbn").on("click", function(){
+    	window.location ="ttbenhnhan.html";
+    });
+    // $("#change").on("click", function(){
+    // 	window.location ="hospital2.html";
+    // });
+
+
+});
