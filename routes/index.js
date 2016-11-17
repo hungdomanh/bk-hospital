31a38f58976f611b43db23081cdbfd9f29c8c0ee
Do Manh Hung <Do Manh Hung>
2016-11-17 16:30:54 +0700

    env fix

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var session = require('express-session');
-var env = require('dotenv').config();
+var env = require('dotenv').config({silent: true});
 
 // connect 
 var connect = process.env.connectString;
